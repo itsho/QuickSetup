5bac9825aa5e09ae99d4806e479faf1a92df1843
itsho <1774797+itsho@users.noreply.github.com>
2017-04-09 17:05:32 +0300

    some UI changes - added enum description and smaller buttons

--- QuickSetup.Logic/Infra/Enums/AllEnums.cs
+++ QuickSetup.Logic/Infra/Enums/AllEnums.cs
@@ -1,11 +1,22 @@
-﻿namespace QuickSetup.Logic.Infra.Enums
+﻿using System.ComponentModel;
+
+namespace QuickSetup.Logic.Infra.Enums
 {
     public enum SoftwareInstallStatusEnum
     {
+        [Description("Unknown")]
         Unknown = 0,
+
+        [Description("Not Installed")]
         NotInstalled = 1,
+
+        [Description("Installed")]
         Installed = 2,
+
+        [Description("Setup File Missing")]
         SetupFileMissing = 3,
+
+        [Description("Unable To Get Status")]
         UnableToGetStatus = 4,
     }
 }
